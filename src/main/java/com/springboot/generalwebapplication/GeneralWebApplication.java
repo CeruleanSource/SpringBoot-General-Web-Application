0f8fbe1e3296da99f47083d4efe53c2a202ada1c
CeruleanSource <abdullah.mohamed@ryerson.ca>
2020-12-21 01:54:37 -0500

    Spring Boot Initialization.

---
+++ src/main/java/com/springboot/generalwebapplication/GeneralWebApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.generalwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/generalwebapplication/GeneralWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.generalwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
